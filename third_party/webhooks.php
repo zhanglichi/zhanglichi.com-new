8b5d777a98fc94273c272c5c6bb85166d75f995e
zhilichi <zhanglichi1991@163.com>
2016-04-08 10:15:21 +0800

    修改github的服务

--- third_party/webhooks.php
+++ third_party/webhooks.php
@@ -7,7 +7,7 @@ $data = [
     'POST'  => $_POST,
 ];
 
-$log_path = '/var/log/three_party/zhanglichi_webhooks' . date("Y-m-d") . '.log';
+$log_path = '/var/log/third_party/zhanglichi_webhooks' . date("Y-m-d") . '.log';
 $command = ' cd /home/web/website/zhanglichi.com/ && git pull origin master >> ' . $log_path;
 
 file_put_contents($log_path, json_encode($data) . "\n", FILE_APPEND);
